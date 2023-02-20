3e08baa30b4d1cde4ba9df637931ef4b4da2d5a7
Viktor Dudnik <jackfilov@gmail.com>
2023-02-20 12:23:29 +0200

    feat: websocket broadcast server demo

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::{net::SocketAddr, collections::HashMap, sync::Arc};
+
+use futures::{StreamExt, TryStreamExt, channel::mpsc::{UnboundedSender, unbounded}, future, SinkExt};
+use tokio::{net::TcpListener, sync::{Mutex, }};
+use tokio_tungstenite::{accept_async, tungstenite::Message};
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+#[tokio::main]
+async fn main() {
+  let bind_addr = "127.0.0.1:9090";
+  let listener =  TcpListener::bind(bind_addr).await
+    .expect(format!("Failed to bind the websocket at {}", bind_addr).as_str());
+  println!("Listening at {}", bind_addr);
+
+  let peer_map = PeerMap::new(Mutex::new(HashMap::new()));
+  while let Ok((stream, addr)) = listener.accept().await {
+    let peer_map = peer_map.clone();
+    tokio::spawn(async move {
+      if let Ok(ws) = accept_async(stream).await {
+        let (tx, rx) = unbounded();
+        let addr = addr;
+        println!("Client connected: {addr}");
+        peer_map.lock().await.insert(addr, tx);
+
+        let (outgoing, incoming) =  ws.split();
+
+        let peer_map_inner = peer_map.clone();
+        let broadcast_future = incoming.try_for_each_concurrent(None, |msg| async {
+          let msg = msg;
+          println!("Received message\n```\n{msg}```\nFrom {addr}");
+          let peers = peer_map_inner.lock().await;
+
+          let clients = peers.iter()
+            .filter(|(peer_addr, _)| 
+              peer_addr != &&addr
+            ).map(|(_, ws_sink)| ws_sink);
+          for mut client in clients {
+            client.send(msg.clone()).await.unwrap();
+          }
+
+          Ok(())
+        });
+
+        let receive_future = rx.map(Ok).forward(outgoing);
+
+        future::select(broadcast_future, receive_future).await;
+
+        println!("Client disconnected: {addr}");
+        peer_map.lock().await.remove(&addr);
+      }
+    });
+  }
+}
